f06f13330bb650cd7f78f223a9a77ddd9af55e05
kristanm1 <kristanm1@gmail.com>
2016-03-10 15:00:03 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,11 +1,18 @@
 window.addEventListener('load', function() {
 	//stran nalozena
-		
+	
+	var izvediPrijavo = function(event) {
+		var ime = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = ime;
+		document.querySelector(".pokrivalo").style.display = "none";
+	}
+	document.querySelector("#prijavniGumb").addEventListener("click", izvediPrijavo);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
